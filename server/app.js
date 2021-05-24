3a6c499e78be721cb2ea50e185ece553af26b909
mosahay <66505462+BhanuPratap10897@users.noreply.github.com>
2021-05-24 23:30:53 +0530

    mongodb connect

--- server/app.js
+++ server/app.js
@@ -1,16 +1,44 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
 const PORT = 5000
+const {MONGOURI} = require('./keys')
 
-const customMiddleware =()=>{
-    console.log("middleware executed !")
-}
+require('./models/user')
 
-app.use(customMiddleware)
+app.use(express.json())
+app.use(require('./routes/auth'))
 
-app.get ('/',(req,res)=>{
-    res.send("Hello Express")
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
 })
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongodb")
+})
+mongoose.connection.on('error',()=>{
+    console.log("err connecting",err)
+})
+
+// const customMiddleware =(req,res,next)=>{
+//     console.log("middleware executed !")
+//     next()
+// }
+// // all page middleware
+// // app.use(customMiddleware)
+
+// app.get ('/',(req,res)=>{
+//     console.log("home")
+//     res.send("Hello Express")
+// })
+
+// //for single page middleware
+// app.get ('/about',customMiddleware,(req,res)=>{
+//     console.log("about")
+//     res.send("About Hello Express")
+// })
 
 app.listen(PORT,()=>{
     console.log("server is running on", PORT)
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Insta-user:Bha876575@cluster0.5zwv4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
