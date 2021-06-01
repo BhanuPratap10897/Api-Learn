35982f64e05f2a4f3ce66179561181867ed549dc
mosahay <66505462+BhanuPratap10897@users.noreply.github.com>
2021-06-01 14:18:08 +0530

    post method solve

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,19 +2,19 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
-3
+
 router.get('/',(req,res)=>{
     res.send("hello")
 })
 
-router.post('./signup',(req,res)=>{
+router.post('/signup',(req,res)=>{
     // console.log(req.body)
-    const {name,email,password} = req.body
-    if(!email || !password || name){
-        res.json({error:"please fill all field"})
+    const {email, password, name} = req.body
+    if(!email || !password || !name){
+        return res.status(422).json({error:"please add all the field"})
     }
     User.findOne({email:email})
-    .then((savesUser)=>{
+    .then((savedUser)=>{
         if(savedUser){
             return res.status(422).json({error:"user already exists with that email"})
         }
